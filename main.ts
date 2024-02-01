27aff95458380396c8815e644d32720a956c6c5b
Niko van Ommen <35970881+nicoripkip@users.noreply.github.com>
2024-02-01 14:29:45 +0100

    Basic setup for sockets

--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
+/**
+ * namespace for the make of sockets
+ * 
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Sockets"
+namespace stadslab 
+{
+    function socket()
+    {
+
+    }
+
+
+    function bind()
+    {
+
+    }
+
+
+    function listen()
+    {
+
+    }
+
+
+    function accept()
+    {
+
+    }
+
+
+    function close()
+    {
+
+    }
+}
+
+//
 basic.forever(function () {
 	
 })
